d8ec97ea8a43b29109f3430b60d97bb005d16b48
0101kvmt <0101kvmt@gmail.com>
2017-07-20 20:18:32 -0700

    Validation service

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ValidateService } from '../../services/validate.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -10,13 +10,32 @@ export class RegisterComponent implements OnInit {
 	username: String;
 	email: String;
 	password: String;
-  constructor() { }
+  constructor(private validateService: ValidateService) { }
 
   ngOnInit() {
   }
 
   onRegisterSubmit(){
-  	console.log(this.name);
+  	const user = {
+  		name: this.name,
+  		username: this.username,
+  		email: this.email,
+  		password: this.password
+  	}
+	  	// Required Fields
+	  if(!this.validateService.validateRegister(user)){
+	  	console.log('Please fill in all fields');
+	  	return false;
+	  }
+
+	  // Validate Email
+	  // if(!this.validateService.validateEmail(user.email)){
+	  // 	console.log('Please use a valid email');
+	  // 	return false;
+	  // }
+
   }
+
+  
 
 }
