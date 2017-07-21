b4b663835f0d9012b1c3bc0cc60e2429e0fdbe83
0101kvmt <0101kvmt@gmail.com>
2017-07-20 20:51:42 -0700

    Reg HTTP post to back-end works

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ValidateService } from '../../services/validate.service';
 import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router'
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -11,7 +13,12 @@ export class RegisterComponent implements OnInit {
 	username: String;
 	email: String;
 	password: String;
-  constructor(private validateService: ValidateService, private flashMessage: FlashMessagesService) { }
+  constructor(
+  	private validateService: ValidateService, 
+  	private flashMessage: FlashMessagesService,
+  	private authService:AuthService,
+  	private router: Router
+  	) { }
 
   ngOnInit() {
   }
@@ -35,6 +42,18 @@ export class RegisterComponent implements OnInit {
 	  // 	return false;
 	  // }
 
+
+	  // Regiser User 
+
+	  this.authService.registerUser(user).subscribe(data => {
+	  	if(data.success){
+	  		this.flashMessage.show('You are now registered and can log in', {cssClass: 'alert-success', timeout: 3000});
+	  		this.router.navigate(['/login']);
+	  	} else {
+	  		this.flashMessage.show('Something went wrong!', {cssClass: 'alert-danger', timeout: 3000});
+	  		this.router.navigate(['/register']);
+	  	}
+	  })
   }
 
   
