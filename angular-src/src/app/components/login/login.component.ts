a16dc644a47902fe270bb8e623c6de9a2b114872
0101kvmt <0101kvmt@gmail.com>
2017-07-24 04:28:12 -0700

    const user AuthService

--- angular-src/src/app/components/login/login.component.ts
+++ angular-src/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username: String;
+  password: String;
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
+  onLoginSubmit(){
+  	const user = {
+  		username: this.username,
+  		password: this.password
+  	}
+
+
+  }
 }
